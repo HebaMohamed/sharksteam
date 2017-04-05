7a279bcd74d7efbeb65dd3b4b788bfd15c333292
dell <dell@DESKTOP-D5F21LL>
2017-04-05 20:36:50 +0200

    helio db changes added

--- src/main/java/com/mycompany/mavenserrr/EventServlet.java
+++ src/main/java/com/mycompany/mavenserrr/EventServlet.java
@@ -16,14 +16,12 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
-import javafx.util.Callback;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import myclassespackage.EventWarning;
-import org.codehaus.jettison.json.JSONObject;
 
 /**
  *
